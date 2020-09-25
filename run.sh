18f2d19fa3181aa138f2b0ae694a02b76d716797
taoyizhou <taoyizhou@meituan.com>
2020-09-25 11:45:56 +0800

    run

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 docker run -p 8501:8501 \
  --mount type=bind,\
-source=/Users/taoyizhou/yizhou/git/serving/tensorflow_serving/servables/tensorflow/testdata/model_out_090110_e,\
+source=/Users/taoyizhou/yizhou/git/serving/tensorflow_serving/servables/tensorflow/testdata/model_out_090121_e,\
 target=/models/half_plus_two \
 -e MODEL_NAME=half_plus_two -t tensorflow/serving
